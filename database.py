ca0f372f4270aafe70aff2d2fd46f16678b551ba
DreamSet69 <159390614+DreamSet69@users.noreply.github.com>
2026-07-28 11:29:11 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,24 +1,23 @@
-import mysql.connector
-from mysql.connector import Error
+import pymysql
 
 def get_connection():
     hostname = "otla5b.h.filess.io"
     database = "pem_web_alsovowel"
-    port = "61030"
+    port = 61030
     username = "pem_web_alsovowel"
     password = "5edd5a20244c7421b121771def4251d6271cf9ef"
 
     try:
-        connection = mysql.connector.connect(
+        connection = pymysql.connect(
             host=hostname, 
             database=database, 
             user=username, 
             password=password, 
             port=port
         )
-        if connection.is_connected():
-            return connection
-            
-    except Error as e:
+        return connection
+    except Exception as e:
         print("Error while connecting to MariaDB", e)
-        return None
+        # We raise the exception so we can see the actual error on Vercel logs, 
+        # or it can be handled by the app.
+        raise e
